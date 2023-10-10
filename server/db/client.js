cffd046ee035342d36db15d01c3f50a3a8fce647
Sofie Christensen <sofiec510@gmail.com>
2023-10-10 15:50:30 -0400

    client for deploying

--- server/db/client.js
+++ server/db/client.js
@@ -3,7 +3,7 @@ const { Client } = require("pg");
 
 //Establishing connect to database (like how we do with http://)
 const dbName = "museumdb";
-const client = new Client(`postgres://localhost:5433/${dbName}`);
-
+// const client = new Client(`postgres://localhost:5433/${dbName}`);
+const client = new Client('postgres://museumdb_pvkq_user:dKSGfjRDWxsY8y2pmoxsx1cji7GREdJm@dpg-ckiqh5kl4vmc73cejr00-a/museumdb_pvkq')
 //Export for use in other files
 module.exports = client;
