9187be9fac785e03c78f16d6487c70ff7ec70c2f
Jennifer <jennifer.y.hong@gmail.com>
2023-10-15 15:34:58 -0500

    images clickable for all museums

--- client/src/components/Museums/GetAllMuseums.jsx
+++ client/src/components/Museums/GetAllMuseums.jsx
@@ -65,11 +65,13 @@ export default function GetAllMuseums() {
             <div key={museum.museumName} className="museum-item-all">
               <h2 className="museum-headers">{museum.museumName}</h2>
               <div className="image-and-text">
-                <img
-                  src={museum.image}
-                  alt={museum.museumName}
-                  className="museum-image"
-                />
+                <a href={`/museums/${museum.museumId}`}>
+                  <img
+                    src={museum.image}
+                    alt={museum.museumName}
+                    className="museum-image"
+                  />
+                </a>
                 <p className="museum-description">{museum.description}</p>
               </div>
               <div className="button-container">
