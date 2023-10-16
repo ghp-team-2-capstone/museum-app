9c53a4aeb1d4763beb962648a15c31937a7be1af
Sofie Christensen <sofiec510@gmail.com>
2023-10-16 14:31:07 -0400

    db tweaks

--- server/db/client.js
+++ server/db/client.js
@@ -3,7 +3,10 @@ const { Client } = require("pg");
 
 //Establishing connect to database (like how we do with http://)
 const dbName = "museumdb";
+
+//use line below if you need to re-seed
 // const client = new Client(`postgres://localhost:5433/${dbName}`);
 const client = new Client('postgres://museumdb_pvkq_user:dKSGfjRDWxsY8y2pmoxsx1cji7GREdJm@dpg-ckiqh5kl4vmc73cejr00-a/museumdb_pvkq')
+
 //Export for use in other files
 module.exports = client;
